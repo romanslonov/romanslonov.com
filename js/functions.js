48184b387cc4e72877593cfb312f5d7b37132f6c
romamilkovic <destabiliseus@gmail.com>
2015-07-31 11:44:41 +0800

    First commit

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+function resizeDiv() {
+    vpw = $(window).width();
+    vph = $(window).height();
+    $(".right").css({
+        height: vph + "px"
+    })
+}
+$(document).ready(function() {
+    resizeDiv()
+});
+window.onresize = function(e) {
+    resizeDiv()
+};
