89dc1c4222307477ec0fb95824d992919d8ac58d
romamilkovic <destabiliseus@gmail.com>
2015-08-02 09:22:27 +0800

    Big update

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,57 @@
+$('document').ready(function(){
+    $('.eng').on('click', function(e){      
+        // отменяем стандартное действие при клике
+        e.preventDefault();
+        // Получаем адрес страницы
+        var href = $(this).attr('href');
+        // Передаем адрес страницы в функцию
+        getContent(href, true);
+    });
+});
+
+// Добавляем обработчик события popstate, 
+// происходящего при нажатии на кнопку назад/вперед в браузере  
+window.addEventListener("popstate", function(e) {
+    // Передаем текущий URL
+    getContent(location.pathname, false);
+});
+
+// Функция загрузки контента
+function getContent(url, addEntry) {
+    $.get(url).done(function(data) {
+        // Обновление только текстового содержимого в сером блоке
+        $('#content').html($(data).find("#content").html());
+        // Если был выполнен клик в меню - добавляем запись в стек истории сеанса
+        // Если была нажата кнопка назад/вперед, добавлять записи в историю не надо
+        if(addEntry == true) {
+            // Добавляем запись в историю, используя pushState
+            history.pushState(null, null, url); 
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function resizeDiv() {
     vpw = $(window).width();
     vph = $(window).height();
@@ -11,3 +65,4 @@ $(document).ready(function() {
 window.onresize = function(e) {
     resizeDiv()
 };
+
