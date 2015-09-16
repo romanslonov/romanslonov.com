254fc0605d7ee38c0805c48ec6746babb0ec00e5
romamilkovic <destabiliseus@gmail.com>
2015-09-17 02:54:53 +0800

    Update 17.09.15

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,30 @@
-
-
-
-
 function resizeDiv() {
     vpw = $(window).width();
     vph = $(window).height();
-    $(".right").css({
-        height: vph + "px"
-    })
+    $(".right").css({ height: vph + "px" })
 }
 $(document).ready(function() {
     resizeDiv()
 });
+
+
+$(document).ready(function() {
+  resizeDiv()
+
+  var el = $("#navigation");
+  el.on("click", "button", function() {
+    $.ajax('russian.html', {
+      success: function(response) {
+        $('.content').html(response).fadeIn();
+      },
+      error: function() {
+        $('.content').html('<li>There was a problem fetching the latest photos. Please try again.</li>');
+      }
+    });
+  });
+});
+
+
 window.onresize = function(e) {
     resizeDiv()
 };
