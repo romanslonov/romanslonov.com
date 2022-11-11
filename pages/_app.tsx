c30cf288a98d6e42d856164d53050080efd236ba
Roman <destabiliseus@gmail.com>
2022-11-11 18:48:31 +0800

    Fixed footer color for dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </main>
         <footer css={tw`py-8`}>
-          <p css={tw`text-center text-gray-500`}>
+          <p css={tw`text-center text-gray-500 dark:text-gray-400`}>
             Made with Next.js, Twin.micro and Tailwindcss.
           </p>
         </footer>
