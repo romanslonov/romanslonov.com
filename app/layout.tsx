3772e375a8f975f01fa56291cbe876d2ba0ba457
Roman Slonov <destabiliseus@gmail.com>
2024-04-21 07:01:53 +0800

    feat: add lil animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Navigation } from '@/app/navigation';
 
 export const metadata: Metadata = {
   title: 'Roman Slonov',
-  description: 'Frontend UI/UX Designer at ServerHub',
+  description: 'Frontend Developer & Designer',
   authors: { name: 'Roman Slonov', url: 'https://romanslonov.com' },
   themeColor: { color: '#000', media: '(prefers-color-scheme: dark)' },
   robots: 'index, follow',
@@ -20,7 +20,7 @@ export default function Layout({ children, ...props }: PropsWithChildren) {
     <html lang="en" className="scroll-smooth">
       {gaTrackingId ? <GoogleAnalytics gaTrackingId={gaTrackingId} /> : null}
       {umamiId ? <UmamiAnalytics umamiId={umamiId} /> : null}
-      <body className="flex min-h-screen flex-col overflow-x-hidden">
+      <body className="flex min-h-screen flex-col overflow-x-hidden animate-blurred-fade-in">
         <div className="flex-1 w-full max-w-3xl p-4 mx-auto space-y-12">
           <Navigation />
           <main className="flex-1">{children}</main>
