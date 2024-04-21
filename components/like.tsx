6542dea34a8cda2b7c875fa49aea87af6f85b18a
Roman Slonov <destabiliseus@gmail.com>
2024-04-21 18:47:59 +0800

    refactor: improve like button

--- components/like.tsx
+++ components/like.tsx
@@ -11,8 +11,12 @@ export function Like({ slug }: { slug: string }) {
     <div className="fixed bottom-4 left-[50%] p-0.5 bg-white dark:bg-neutral-900 dark:border-neutral-800 translate-x-[-50%] border shadow-xl font-medium text-sm rounded-full">
       <button
         type="button"
+        disabled={isCurrentUserLike || isLoading}
         onClick={() => like()}
-        className="flex items-center gap-1 rounded-full pl-2 pr-4 py-1.5 hover:bg-neutral-100 dark:hover:bg-neutral-800"
+        className={cn(
+          'flex items-center gap-1 rounded-full pl-2 pr-4 py-1.5',
+          isCurrentUserLike ? '' : 'hover:bg-neutral-100 dark:hover:bg-neutral-800',
+        )}
       >
         <HeartIcon
           className={cn(
@@ -22,7 +26,7 @@ export function Like({ slug }: { slug: string }) {
         />
         {isLoading ? '...' : likes ?? '-'}
         <div className="h-4 mx-1 w-px bg-neutral-300 dark:bg-neutral-700"></div>
-        This was helpful?
+        {isCurrentUserLike ? 'Thanks!' : 'This was helpful?'}
       </button>
     </div>
   );
