f1c8b5ff138b8d1ed0431c00294aec4c16279cf1
Roman Slonov <destabiliseus@gmail.com>
2025-01-27 16:02:38 +0800

    refactor: update umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css';
 import GoogleAnalytics from '@/app/google-analytics';
-import UmamiAnalytics from './umami-analytics';
 import { type PropsWithChildren } from 'react';
 import { type Metadata } from 'next';
 import { Navigation } from '@/app/navigation';
 import { baseUrl } from './sitemap';
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: 'Roman Slonov',
@@ -29,13 +29,16 @@ export const metadata: Metadata = {
   },
 };
 
-export default function Layout({ children, ...props }: PropsWithChildren) {
+export default function Layout({ children }: PropsWithChildren) {
   const gaTrackingId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
-  const umamiId = process.env.NEXT_PUBLIC_UMAMI_ID;
   return (
     <html lang="en" className="scroll-smooth">
       {gaTrackingId ? <GoogleAnalytics gaTrackingId={gaTrackingId} /> : null}
-      {umamiId ? <UmamiAnalytics umamiId={umamiId} /> : null}
+      <Script
+          strategy="lazyOnload"
+          src="https://umami.romanslonov.com/script.js"
+          data-website-id="f497bc3b-07d7-47b9-83d8-9dac4b9a1fdb"
+      />
       <body className="flex min-h-screen flex-col overflow-x-hidden animate-blurred-fade-in">
         {/* max-w-3xl */}
         <div className="flex-1 w-full p-4 mx-auto space-y-12">
